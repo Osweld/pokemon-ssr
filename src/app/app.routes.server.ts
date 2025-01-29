1191c90432722980c05083b4de8c29d08243e027
German Reyes <oswaldo101.gr@gmail.com>
2025-01-29 08:22:11 -0600

    fix angular manifest error

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,21 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'pokemons/page/:page',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [{page: "1"},{page: "2"},{page: "3"},{page: "4"},{page: "5"},{page: "6"}]
+    },
+  },{
+    path: 'pokemons/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const pokemonNameList = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${151}`)
+  .then((res) => res.json());
+      return pokemonNameList.results.map((pokemon: any) => ({id: pokemon.name}))
+    }
+  },{
     path: '**',
     renderMode: RenderMode.Prerender
-  }
+  },
 ];
